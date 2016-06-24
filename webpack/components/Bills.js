020be74bb9b90077a963dd3eb6b177d4f6d974d7
Brant Wright <bwright984@gmail.com>
2016-06-24 15:51:06 -0600

    nav bar and bills component

---
+++ webpack/components/Bills.js
@@ -0,0 +1,31 @@
+import React from 'react'; 
+
+class BeerCards extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { bills: [] };
+  }
+  componentWillMount() {
+  	$.ajax({
+  		url: '/api/bills', 
+  		type: 'GET', 
+  		dataType: 'JSON'
+  	}).done( bills => {
+  		this.setState({ bills }); 
+  	}).fail( data => {
+  		console.log(data); 
+  	}); 
+  }
+
+
+  render() {
+    return(
+      <div>
+        {this.}
+
+    )
+  }
+
+
+
+export default Bills; 
--- webpack/containers/App.js
+++ webpack/containers/App.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const App = ({ children }) => (
   <div>
-    Hello World
+  	<nav>
+  	   <div className="nav-wrapper">
+  	     <a href="#" className="brand-logo">Personal Budget</a>
+  	     <ul id="nav-mobile" className="right hide-on-med-and-down">
+  	       <li><a href="sass.html">Profile</a></li>
+  	       <li><a href="badges.html">Bills</a></li>
+  	       <li><a href="collapsible.html">Budgets</a></li>
+  	     </ul>
+  	   </div>
+  	 </nav>
     { children }
   </div>
+  	
 )
 
 export default App;

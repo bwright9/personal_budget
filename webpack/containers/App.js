1197c2aed910f92fe7f43bb9939b5ee7c0bf9ca4
Brant Wright <bwright984@gmail.com>
2016-06-26 12:18:49 -0600

    added footer and icon

--- webpack/containers/App.js
+++ webpack/containers/App.js
@@ -14,9 +14,24 @@ const App = ({ children }) => (
   	   </div>
   	 </nav>
     { children }
-  </div>
-  	
+        <footer className="page-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col l6 s12">
+                <p className="grey-text text-lighten-4"></p>
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2016 Copyright Text
+            </div>
+          </div>
+        </footer>
+      </div>
 )
 
 export default App;
+
+
 
